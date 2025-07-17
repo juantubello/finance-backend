301195fd2fabda1f492018abd103cdc01f40d4af
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2025-07-16 23:52:07 -0300

    some changes

--- models/expense.go
+++ models/expense.go
@@ -5,8 +5,8 @@ import "time"
 type Expenses struct {
 	ID          uint      `gorm:"primaryKey" json:"id"`
 	UUID        string    `gorm:"unique" json:"uuid"`
-	DateTime    string    `json:"date_time"`                 // formato: "2025-07-08 12:00:00"
-	Date        time.Time `json:"date" gorm:"type:datetime"` // Nuevo campo de tipo fecha
+	DateTime    string    `json:"date_time"`
+	Date        time.Time `json:"date" gorm:"type:datetime"`
 	Description string    `json:"description"`
 	Amount      float64   `json:"amount"`
 	Type        string    `json:"type"`
