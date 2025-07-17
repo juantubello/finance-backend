e8ba80c682b122ccaf637e9b9e0a021b3bae6324
Juan Pablo Fernandez Tubello <juanfernatubello@gmail.com>
2025-07-17 00:26:47 -0300

    adding file path and files list on directory

--- controllers/cards/cards_handler.go
+++ controllers/cards/cards_handler.go
@@ -1,17 +1,24 @@
 package cards
 
 import (
-	"finance-backend/models"
+	"finance-backend/config"
+	cards "finance-backend/controllers/base"
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
-
-	cards "finance-backend/controllers/base"
 )
 
 type BalanceController struct {
 	*cards.BaseController // Embed base to share base methods
+}
+
+type resumePaths struct {
+	cardLogo string
+	filePath string
+	fileName string
 }
 
 func NewCardsController() *BalanceController {
@@ -22,8 +29,47 @@ func NewCardsController() *BalanceController {
 
 func (ec *BalanceController) SyncResumes(c *gin.Context) {
 
-	var cards models.Expenses
-	fmt.Println(cards)
+	resumesPath := getResumesFilePath()
 
-	c.JSON(http.StatusOK, gin.H{"test": 10})
+	c.JSON(http.StatusOK, gin.H{"test": resumesPath})
+}
+
+func getResumesFilePath() []resumePaths {
+
+	var resumes []resumePaths
+	//add len to directories
+	directories := make([]string, 2)
+
+	directories = append(directories, config.GetEnv("CARD_VISA_PATH"))
+	directories = append(directories, config.GetEnv("CARD_MASTERCARD_PATH"))
+
+	for _, dir := range directories {
+
+		entries, err := os.ReadDir(dir)
+		if err != nil {
+			//  log.Fatal(err)
+		}
+
+		//var files []string
+		for _, v := range entries {
+			if filepath.Ext(v.Name()) != ".pdf" {
+				continue
+			}
+			//completePath := fmt.Sprintf("%s/%s", dir, v.Name())
+			completePath := dir + "/" + v.Name()
+			fmt.Println(completePath)
+			//		if v.IsDir() {
+			//			continue
+			//		}
+			//		if filepath.Ext(v.Name()) != ".md" {
+			//			continue
+			//		}
+			//		files = append(files, filepath.Join(dir, v.Name()))
+		}
+
+	}
+
+	//return files
+
+	return resumes
 }
